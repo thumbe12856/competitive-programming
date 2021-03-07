845608b31179ea342de59635a96757590cd0e3e3
thumbd12856 <thumbd12856@gmail.com>
2021-03-08 00:51:14 +0800

    [DP]

--- cf/DP/1400 Constanze's Machine/solve.py
+++ cf/DP/1400 Constanze's Machine/solve.py
@@ -1,9 +1,23 @@
-
 def solve(S):
     if "m" in S or "w" in S:
         return 0
 
+    N = len(S)
+    last_val = 1
+    last_last_val = 1
+    for i in range(N - 2, -1, -1):
+        curr_val = last_val
+        if i + 1 < N:
+            if S[i] == "u" and S[i + 1] == "u":
+                curr_val += last_last_val
+
+            elif S[i] == "n" and S[i + 1] == "n":
+                curr_val += last_last_val
+
+        last_last_val = last_val
+        last_val = curr_val
+
+    return last_val % (10 ** 9 + 7)
 
 S = input()
 print(solve(S))
-
