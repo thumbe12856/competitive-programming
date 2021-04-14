0220e46648dcdecdded5fe6c0049bc5186298187
thumbd12856 <thumbd12856@gmail.com>
2021-04-14 22:16:17 +0800

    [cf] 1500 Division

---
+++ cf/1500 Division/solve.py
@@ -0,0 +1,28 @@
+def solve(P, Q):
+    can = []
+    q = Q
+    i = 2
+    while i ** 2 <= q:
+        if q % i == 0:
+            can.append(i)
+            while q % i == 0:
+                q //= i
+        i += 1
+
+    if q > 1:
+        can.append(q)
+
+    ans = 0
+    for c in can:
+        p = P
+        while p % Q == 0:
+            p //= c
+
+        ans = max(ans, p)
+
+    return ans
+
+T = int(input())
+for _ in range(T):
+    P, Q = map(int, input().split())
+    print(solve(P, Q))
