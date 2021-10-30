eef385d3a5cced0ce8a9bab5b374a4b15e8be87a
thumbd12856 <thumbd12856@gmail.com>
2021-10-30 11:39:29 +0800

    [template] add python

---
+++ _templates/template.py
@@ -0,0 +1,9 @@
+import sys
+def get_ints(): return list(map(int, sys.stdin.readline().strip().split()))
+sys.setrecursionlimit(10000)
+
+
+T = int(input())
+for _ in range(T):
+    N, M, K = int(get_ints())
+
